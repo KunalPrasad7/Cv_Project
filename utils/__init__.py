1418f62fe838e02e477cab5c2f75a472c91d7a04
KunalPrasad7 <140092390+KunalPrasad7@users.noreply.github.com>
2025-11-07 01:49:17 +0530

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,6 @@
+# Utils package
+from .detection import HumanDetector
+from .motion import MotionAnalyzer
+from .alerts import AlertSystem
+
+__all__ = ['HumanDetector', 'MotionAnalyzer', 'AlertSystem']
